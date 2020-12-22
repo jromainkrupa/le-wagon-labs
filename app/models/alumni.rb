ccd52be2e65aeec50e2f8db216ca17dbfca0b966
Abdulmelik SAK <melik.sak@icloud.com>
2020-12-22 10:30:14 +0100

    validation alumni et entrepreneur

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -3,4 +3,12 @@ class Alumni < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tickets,     dependent: :destroy
+  has_many :articles,    dependent: :destroy
+  has_many :candidacies, dependent: :destroy
+  has_many :specialties, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
 end
--- app/models/entrepreneur.rb
+++ app/models/entrepreneur.rb
@@ -3,4 +3,10 @@ class Entrepreneur < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :projects
+
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :phone,      presence: true
 end
