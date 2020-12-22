ee53eee8309e43cd20fc45095acfdb608a4117d5
Abdulmelik SAK <melik.sak@icloud.com>
2020-12-22 23:32:05 +0100

    validates dans ticket car plusieurs validations en une ligne

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :alumni
   belongs_to :project
 
-  validate :description, presence: true, length: { minimum: 100 }
+  validates :description, presence: true, length: { minimum: 100 }
 end
