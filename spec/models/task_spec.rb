99b5edd2683d5066663d6fde300f68cfc67fdc6a
Abdulmelik SAK <melik.sak@icloud.com>
2020-12-25 19:36:17 +0100

    ajout validation presence name dans task + test associatione t validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
+describe Task do
+  context 'test des associations' do
+    it { should belong_to(:project) }
+  end
+end
+
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(build(:task)).to be_valid
+  end
+
+  it 'is not valid without name' do
+    expect(build(:task_name_empty)).not_to be_valid
+  end
 end
