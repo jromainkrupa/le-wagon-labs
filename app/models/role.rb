e3c86d3100b37d49680718efa0b6f6ba5f9a7958
Abdulmelik SAK <melik.sak@icloud.com>
2020-12-25 19:32:40 +0100

    ajout validations dans role + test validation et associations role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ApplicationRecord
   belongs_to :project
   has_many :candidacies
+
+  validates :name, :number, :status, presence: true
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
+describe Role do
+  context 'test des associations' do
+    it { should belong_to(:project) }
+    it { should have_many(:candidacies) }
+  end
+end
+
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(build(:role)).to be_valid
+  end
+
+  it 'is not valid without name' do
+    expect(build(:role_name_empty)).not_to be_valid
+  end
 end
