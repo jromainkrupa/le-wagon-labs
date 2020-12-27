cd3280cead6d3baf700373139d07ada1666f4caf
Thomas <th.farre@gmail.com>
2020-12-27 13:06:08 +0100

    added a pages controller with home page, started to designed navbar and toggleable menu, added stimulus and toggeable menu is working properly

--- Gemfile
+++ Gemfile
@@ -60,3 +60,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "devise", "~> 4.7"
 
 gem "i18n", "~> 1.8"
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require("channels")
 // or the `imagePath` JavaScript helper below.
 // import tailwind
 import '../stylesheets/application.scss';
+import '../controllers'
 
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
