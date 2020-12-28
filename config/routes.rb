1e51c2694f3df8c55154663d730d3bfa2fab0021
Abdulmelik SAK <melik.sak@icloud.com>
2020-12-28 11:51:23 +0100

    ajouts des routes toutes les routes necessite finitions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,26 @@ Rails.application.routes.draw do
   devise_for :alumnis
   devise_for :entrepreneurs
 
+  resources :alumnis do
+    collection do
+      get 'dashboard', to: 'alumnis#dashboard'
+    end
+  end
+
+  resources :entrepreneurs do
+    collection do
+      get 'dashboard', to: 'entrepreneurs#dashboard'
+    end
+  end
+
   root to: 'pages#home'
+
+  resources :projects do
+    resources :tasks
+    resources :roles do
+      resources :candidacies
+    end
+  end
+
+  resources :articles
 end
