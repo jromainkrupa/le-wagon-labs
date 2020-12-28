c71e5a760a4cd61c719767a4a343fb32db67acb9
Abdulmelik SAK <melik.sak@icloud.com>
2020-12-28 12:08:31 +0100

    routes faites rapidement peut etre a revoir

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :alumnis
   devise_for :entrepreneurs
 
-  resources :alumnis do
+  resources :alumnis, only: [:index, :show, :update, :edit] do
     collection do
       get 'dashboard', to: 'alumnis#dashboard'
     end
   end
 
-  resources :entrepreneurs do
+  resources :entrepreneurs, only: [:index, :show, :update, :edit] do
     collection do
       get 'dashboard', to: 'entrepreneurs#dashboard'
     end
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :projects do
+  resources :projects, expect: :destroy do
     resources :tasks
     resources :roles do
       resources :candidacies
