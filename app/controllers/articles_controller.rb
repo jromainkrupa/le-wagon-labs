8d8f9b3c97fe6cfe016a044273f6f6c5ad7088c7
Gagou <gaelle.shearn.jmr@gmail.com>
2021-01-05 12:27:44 +0100

    un peu plus de pundit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,19 @@
 class ArticlesController < ApplicationController
-  before_action policy_scope(Article)
 
   def index
-    @articles = Article.all
+    @articles = policy_scope(Article)
   end
 
   def show
+    @article = policy_scope(Article).find(params[:id])
   end
 
   def create
   end
 
   def new
+    @article = Article.new
+    authorize @article
   end
 
   def update
