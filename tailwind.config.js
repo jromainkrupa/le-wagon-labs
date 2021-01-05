565bbe5e7109e2723dbe9a1f4e3c855317d59c5c
Thomas <th.farre@gmail.com>
2021-01-05 10:57:07 +0100

    add footer on mobile, home page, color in config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+            '50': '#fdf5f5',
+            '100': '#fbebeb',
+            '200': '#f4cccc',
+            '300': '#edadad',
+            '400': '#e07070',
+            '500': '#d23333',
+            '600': '#bd2e2e',
+            '700': '#9e2626',
+            '800': '#7e1f1f',
+            '900': '#671919'
+        },
+      },
+    },
   },
   variants: {
     extend: {},
