4b7a3fc88b2742184ca80709c07701a504de5eaa
Abdulmelik SAK <melik.sak@icloud.com>
2021-01-06 10:50:38 +0100

    policy et controller articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,22 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = policy_scope(Article)
+    authorize @articles
   end
 
   def show
     @article = policy_scope(Article).find(params[:id])
+    authorize @article
   end
 
   def create
+    @article = Article.new(article_params)
+    authorize @article
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
   def new
@@ -17,11 +26,29 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article = policy_scope(Article).find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
 
   def edit
+    @article = policy_scope(Article).find(params[:id])
+    authorize @article
   end
 
   def destroy
+    @article = policy_scope(Article).find(params[:id])
+    authorize @article
+    @article.destroy
+    redirect_to dashboard_alumnis_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
