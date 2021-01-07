a415a37de91822945cbf187591587b49fa96e5d9
Gagou <gaelle.shearn.jmr@gmail.com>
2021-01-07 10:46:31 +0100

    pundit pour les alumnis + migration ajout About

--- app/controllers/alumnis_controller.rb
+++ app/controllers/alumnis_controller.rb
@@ -1,9 +1,36 @@
 class AlumnisController < ApplicationController
-  skip_before_action :authenticate_alumni! || :authenticate_entrepreneur!
+  # skip_before_action :authenticate_alumni! || :authenticate_entrepreneur!, only: [:index]
 
   def dashboard
   end
 
+  def index
+    @alumnis = policy_scope(Alumni)
+    authorize @alumnis
+  end
+
   def show
+    @alumni = policy_scope(Alumni).find(params[:id])
+    authorize @alumni
+  end
+
+  def edit
+    @alumni = policy_scope(Alumni).find(params[:id])
+    authorize @alumni
+  end
+
+  def update
+    @alumni = Alumni.find(params[:id])
+    if @alumni.update(alumni_params)
+      redirect_to alumni_path(@alumni)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def alumni_params
+    params.require(:alumni).permit(:first_name, :last_name, :about)
   end
 end
