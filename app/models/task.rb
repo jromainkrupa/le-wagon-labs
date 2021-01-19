7e494b8b73a2062b466813deb84370ae4534ed44
Abdulmelik SAK <melik.sak@icloud.com>
2021-01-19 10:13:52 +0100

    ajout de la methode completed! pour les tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :project
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }, allow_nil: true
+
+  def completed!
+    self.completed = !completed
+    save
+  end
 end
