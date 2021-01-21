a3f200e2a69bd4ee0eb842a0f32d93a524f3b0e2
Thomas <th.farre@gmail.com>
2021-01-21 15:22:36 +0100

    Todo list front ok

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -47,7 +47,11 @@ class ProjectsController < ApplicationController
   def update
     @project = Project.find(params[:id])
     if @project.update(project_params)
-      redirect_to project_path(@project)
+      if @project.accepted?
+        redirect_to project_path(@project)
+      else
+        redirect_to edit_project_path(@project)
+      end
     else
       render :edit
     end
