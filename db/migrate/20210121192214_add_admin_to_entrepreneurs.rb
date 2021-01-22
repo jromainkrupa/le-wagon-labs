f0aed66926457e2643c691ef6c0cb453f489c175
Abdulmelik SAK <melik.sak@icloud.com>
2021-01-22 10:45:23 +0100

    mise en place de administrate

--- db/migrate/20210121192214_add_admin_to_entrepreneurs.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToEntrepreneurs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :entrepreneurs, :admin, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_192214) do
+ActiveRecord::Schema.define(version: 2021_01_21_090600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2021_01_21_192214) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "about"
     t.string "city"
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_alumnis_on_email", unique: true
     t.index ["reset_password_token"], name: "index_alumnis_on_reset_password_token", unique: true
   end
@@ -86,7 +85,6 @@ ActiveRecord::Schema.define(version: 2021_01_21_192214) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "about"
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_entrepreneurs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_entrepreneurs_on_reset_password_token", unique: true
   end
