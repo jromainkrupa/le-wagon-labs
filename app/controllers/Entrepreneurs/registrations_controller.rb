120cc2e1a3ae7de1ad697877ad57fe23d02b8b09
Thomas <th.farre@gmail.com>
2021-01-26 15:13:22 +0100

    after sign up des entrepreneurs redirection ok sur project new

--- app/controllers/Entrepreneurs/registrations_controller.rb
+++ app/controllers/Entrepreneurs/registrations_controller.rb
@@ -53,9 +53,9 @@ class Entrepreneurs::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    '/projects/new'
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
